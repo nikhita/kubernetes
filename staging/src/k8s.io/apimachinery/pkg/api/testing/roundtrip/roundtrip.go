abd523520da7d2857b3b8a9e952839ad13df4a6b
Nikhita Raghunath <nikitaraghunath@gmail.com>
2017-08-03 13:07:19 +0530

    Fix roundtrip error message

--- staging/src/k8s.io/apimachinery/pkg/api/testing/roundtrip/roundtrip.go
+++ staging/src/k8s.io/apimachinery/pkg/api/testing/roundtrip/roundtrip.go
@@ -376,7 +376,7 @@ func roundTrip(t *testing.T, scheme *runtime.Scheme, codec runtime.Codec, object
 	// NOTE: we use the encoding+decoding here as an alternative, guaranteed deep-copy to compare against.
 	fuzzer.ValueFuzz(object)
 	if !apiequality.Semantic.DeepEqual(original, obj3) {
-		t.Errorf("%v: fuzzing a copy altered the original, diff: %v", name, diff.ObjectReflectDiff(original, object))
+		t.Errorf("%v: fuzzing a copy altered the original, diff: %v", name, diff.ObjectReflectDiff(original, obj3))
 		return
 	}
 }
